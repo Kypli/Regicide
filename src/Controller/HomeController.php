7fd4b87413f8018c1c416b4ac6cdfe147cc8c7ca
Pierre <pierre.amboise@yahoo.fr>
2023-01-14 21:00:44 +0100

    home index

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\Date as DateSer;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/", name="home")
+ */
+class HomeController extends AbstractController
+{
+	private $dateSer;
+
+	public function __construct(DateSer $dateSer)
+	{
+		$this->dateSer = $dateSer;
+	}
+
+	/**
+	 * @Route("/", name="")
+	 */
+	public function index(Request $request){
+
+		// User
+		$user = $this->getUser();
+		$user_id = $user != null ? $user->getId() : 0;
+
+		return $this->render('home/index.html.twig',[
+
+			// Calendrier
+			'dateJour' => $this->dateSer->dateToFrench('now', 'l j F Y'),
+		]);
+	}
+}
